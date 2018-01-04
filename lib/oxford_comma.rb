b640ce2c2443dc1c3539bb1d2d1677f2d1245896
York Wang <yw544@nyu.edu>
2018-01-04 20:24:41 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,13 @@
 def oxford_comma(array)
+if array.length == 1
+   array.join
 
+   if array.length == 2
+     array.join(" and ")
+
+
+     if array.length >= 3
+array.last.insert(0, "and ")
+array.join(", ")
+end
 end
