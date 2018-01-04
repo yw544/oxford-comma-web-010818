4bcce653d9ba355d1c032e748f451096eb030f3a
York Wang <yw544@nyu.edu>
2018-01-04 20:25:59 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,13 @@
 def oxford_comma(array)
+if array.length == 1
+   array.join
 
+ elsif array.length == 2
+     array.join(" and ")
+
+
+   elsif array.length >= 3
+array.last.insert(0, "and ")
+array.join(", ")
+end
 end
