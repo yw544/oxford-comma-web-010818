169a5cef834cb0a52238c32d1a214de278aa403a
York Wang <yw544@nyu.edu>
2018-01-04 20:25:24 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,11 +2,11 @@ def oxford_comma(array)
 if array.length == 1
    array.join
 
-   if array.length == 2
+ elsif array.length == 2
      array.join(" and ")
 
 
-     if array.length >= 3
+   elsif array.length >= 3
 array.last.insert(0, "and ")
 array.join(", ")
 end
